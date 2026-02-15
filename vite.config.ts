bc249605e5e2dcfaa400f80ed7f65976b283d7fa
Usuario <usuario@example.com>
2026-02-14 20:06:17 -0400

    Revert base path for subdomain deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/yo-tambien-2026/',
+    base: '/',
     server: {
       port: 3000,
       host: '0.0.0.0',
